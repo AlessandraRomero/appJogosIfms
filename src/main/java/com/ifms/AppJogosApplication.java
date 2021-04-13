3311f30433ff79a1119752540e33b4c62dc3b35d
AlessandraRomero <alessandraromeiro098@gmail.com>
2021-04-13 18:46:24 -0400

    first commit

---
+++ src/main/java/com/ifms/AppJogosApplication.java
@@ -0,0 +1,13 @@
+package com.ifms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppJogosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppJogosApplication.class, args);
+	}
+
+}
