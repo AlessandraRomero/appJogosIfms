6d47aac4cbaa105dd17d4012d3b0f92b737e661b
AlessandraRomero <alessandraromeiro098@gmail.com>
2021-04-13 21:51:17 -0400

    conexão com BD

---
+++ src/main/java/com/ifms/services/ModalidadeService.java
@@ -0,0 +1,26 @@
+package com.ifms.services;
+
+import java.text.Collator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ifms.dto.ModalidadeDTO;
+import com.ifms.entities.Modalidade;
+import com.ifms.repositories.ModalidadeRepository;
+
+@Service
+public class ModalidadeService {
+
+	private ModalidadeRepository repository;
+
+	@Transactional(readOnly = true)
+	public List<ModalidadeDTO> findAll() {
+		List<Modalidade> list = repository.findAll();
+		return list.stream().map(modalidade -> new ModalidadeDTO(modalidade))
+				.collect(Collectors.toList());
+	}
+	
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO tb_modalidade (descricao) VALUES ('Futsal')
+INSERT INTO tb_modalidade (descricao) VALUES ('Volei')
+INSERT INTO tb_modalidade (descricao) VALUES ('Handbal')
+INSERT INTO tb_modalidade (descricao) VALUES ('Basquete')
+INSERT INTO tb_modalidade (descricao) VALUES ('Atletismo')
