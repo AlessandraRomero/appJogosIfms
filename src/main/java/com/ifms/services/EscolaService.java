40ab0b3b2c9cd33be375acce1384b541a3c569a7
AlessandraRomero <alessandraromeiro098@gmail.com>
2021-04-14 21:09:03 -0400

    implementação dos métodos update e delete da entidade Escola

--- src/main/java/com/ifms/services/EscolaService.java
+++ src/main/java/com/ifms/services/EscolaService.java
@@ -4,13 +4,19 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ifms.dto.EscolaDTO;
 import com.ifms.entities.Escola;
 import com.ifms.repositories.EscolaRepository;
+import com.ifms.services.exceptions.DataBaseException;
+import com.ifms.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class EscolaService {
@@ -28,7 +34,52 @@ public class EscolaService {
 	@Transactional(readOnly = true)
 	public EscolaDTO findById(Long id) {
 		Optional<Escola> obj = repository.findById(id);
-		Escola escola = obj.get();
-		return new EscolaDTO();
+		Escola escola = obj.orElseThrow(() -> new ResourceNotFoundException(
+				"A escola solicitada não foi localizada"));
+		return new EscolaDTO(escola);
 	}
+
+	@Transactional
+	public EscolaDTO insert(EscolaDTO dto) {
+		Escola entity = new Escola();
+		entity.setNome(dto.getNome());
+		entity.setEndereco(dto.getEndereco());
+		entity.setSite(dto.getSite());
+		entity.setEmail(dto.getEmail());
+		entity.setDiretor(dto.getDiretor());
+		entity.setTelefone(dto.getTelefone());
+		entity = repository.save(entity);
+		return new EscolaDTO(entity);
+	}
+
+	@Transactional
+	public EscolaDTO update(Long id, EscolaDTO dto) {
+		try {
+			Escola entity = repository.getOne(id);
+			entity.setNome(dto.getNome());
+			entity.setEndereco(dto.getEndereco());
+			entity.setSite(dto.getSite());
+			entity.setEmail(dto.getEmail());
+			entity.setDiretor(dto.getDiretor());
+			entity.setTelefone(dto.getTelefone());
+			entity = repository.save(entity);
+			return new EscolaDTO(entity);
+			
+		}catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("O id da escola não foi localizada");
+	     	}
+		}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}catch( EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("O id da escola não foi localizada");
+		}catch ( DataIntegrityViolationException e) {
+			throw new DataBaseException("Não foi possível deletar a escola, pois a mesma está em uso");
+		}
+		
+		
+	}
+		
 }
